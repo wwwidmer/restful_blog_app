53d837e89814e1e7ae262be26366471470aa091b
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 19:40:25 -0400

    rename from scaffold

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello'
+    'restful_blog'
 )
 
 MIDDLEWARE_CLASSES = (
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -3,14 +3,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+import restful_blog.views
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
+    url(r'^$', restful_blog.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 ]

c0d43ecc8f747d72458df4adca842b1a64e11109
wwwidmer <williamwidmerr@gmail.com>
2017-10-04 00:16:48 -0400

    format a timestamp

--- restful_blog/views.py
+++ restful_blog/views.py
@@ -8,10 +8,15 @@ def index(request):
     return render(request, 'index.html')
 
 
+def format_timestamp(dt):
+	return dt.strftime('%d, %b, %Y')
+
+
 def get_latest_post(request):
+	timestamp = format_timestamp(datetime.utcnow())
 	response = JsonResponse(json.dumps({
 			'title': 'Test Title!',
-			'timestamp': datetime.utcnow(),
+			'timestamp': timestamp,
 			'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
 			'thumbnail': 'https://deadlink.com',
 			'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -21,20 +26,21 @@ def get_latest_post(request):
 
 
 def get_all_posts(request, page=0):
+	timestamp = format_timestamp(datetime.utcnow())
 	response = JsonResponse(json.dumps({
 			'total': 2,
 			'page': page,
 			'posts': {
 				{
 					'title': 'Test Title!',
-					'timestamp': datetime.utcnow(),
+					'timestamp': timestamp,
 					'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
 					'thumbnail': 'https://deadlink.com',
 					'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 				},
 				{
 					'title': 'Test, slightly older Title!',
-					'timestamp': datetime.utcnow(),
+					'timestamp': timestamp,
 					'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
 					'thumbnail': 'https://deadlink.com',
 					'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'

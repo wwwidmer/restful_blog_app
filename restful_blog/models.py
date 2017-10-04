78aca106cc26efe5f76fc08c93f456b05b87de6d
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 20:40:40 -0400

    better language, dummy data

--- restful_blog/models.py
+++ restful_blog/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 
 class Post(models.Model):
-	pass
+	title = ''
+	timestamp = ''
+	description = ''
+	thumbnail = ''
+	text = ''
--- restful_blog/views.py
+++ restful_blog/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 
-def get_latest_entry(request):
-	return HttpResponse(200)
+def get_latest_post(request):
+	response = JsonResponse(
+		'title': 'Test Title!',
+		'timestamp': 'Oct 17th 2017',
+		'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
+		'thumbnail': 'https://deadlink.com',
+		'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	)
+	return response
 
 
-def paginated_entries(request):
-	return HttpResponse(200)
+def get_all_posts(request, page=0):
+	response = JsonResponse(
+		'total': 2,
+		'page': page,
+		'posts': {
+			{
+				'title': 'Test Title!',
+				'timestamp': 'Oct 17th 2017',
+				'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
+				'thumbnail': 'https://deadlink.com',
+				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			},
+			{
+				'title': 'Test, slightly older Title!',
+				'timestamp': 'Oct 16th 2017',
+				'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
+				'thumbnail': 'https://deadlink.com',
+				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+			},
+		}
+	)
+	return response

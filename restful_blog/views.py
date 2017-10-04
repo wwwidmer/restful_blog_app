2beca484875547fba43008b5b64af3927ca77349
wwwidmer <williamwidmerr@gmail.com>
2017-10-04 00:18:51 -0400

    safe=false when json serializing and returning a response

--- restful_blog/views.py
+++ restful_blog/views.py
@@ -20,7 +20,7 @@ def get_latest_post(request):
 			'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
 			'thumbnail': 'https://deadlink.com',
 			'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-		})
+		}, safe=False)
 	)
 	return response
 
@@ -46,6 +46,6 @@ def get_all_posts(request, page=0):
 					'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 				},
 			}
-		})
+		}, safe=False)
 	)
 	return response

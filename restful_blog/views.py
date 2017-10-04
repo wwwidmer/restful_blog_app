e06e0045f1c21fb34f55f4184fcad9967b043326
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 21:03:26 -0400

    i need a linter so bad

--- restful_blog/views.py
+++ restful_blog/views.py
@@ -19,24 +19,25 @@ def get_latest_post(request):
 
 
 def get_all_posts(request, page=0):
-	response = JsonResponse(
-		'total': 2,
-		'page': page,
-		'posts': {
-			{
-				'title': 'Test Title!',
-				'timestamp': 'Oct 17th 2017',
-				'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
-				'thumbnail': 'https://deadlink.com',
-				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-			},
-			{
-				'title': 'Test, slightly older Title!',
-				'timestamp': 'Oct 16th 2017',
-				'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
-				'thumbnail': 'https://deadlink.com',
-				'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-			},
+	response = JsonResponse({
+			'total': 2,
+			'page': page,
+			'posts': {
+				{
+					'title': 'Test Title!',
+					'timestamp': 'Oct 17th 2017',
+					'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
+					'thumbnail': 'https://deadlink.com',
+					'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+				},
+				{
+					'title': 'Test, slightly older Title!',
+					'timestamp': 'Oct 16th 2017',
+					'description': 'Wow! This is a description, but we\'ve got a lot more to say in the actual post!',
+					'thumbnail': 'https://deadlink.com',
+					'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+				},
+			}
 		}
 	)
 	return response

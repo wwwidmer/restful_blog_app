802d042836162edb6c1005310bafb94e9a90de44
wwwidmer <williamwidmerr@gmail.com>
2017-10-03 21:07:30 -0400

    whoops not a regex

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -7,7 +7,7 @@ import restful_blog.views
 
 urlpatterns = [
     url(r'^$', restful_blog.views.index, name='index'),
-    url('r^latest/', restful_blog.views.get_latest_post, name='latest'),
-    url('r^posts/', restful_blog.views.get_all_posts, name='all'),
+    url(r'^latest/', restful_blog.views.get_latest_post, name='latest'),
+    url(r'^posts/', restful_blog.views.get_all_posts, name='all'),
     url(r'^admin/', include(admin.site.urls)),
 ]

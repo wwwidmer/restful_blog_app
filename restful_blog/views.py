6e6aa562adc06ea3d9fbba5dc0a69171aa3ef628
wwwidmer <williamwidmerr@gmail.com>
2017-10-05 17:39:09 -0400

    tab space

--- restful_blog/views.py
+++ restful_blog/views.py
@@ -55,4 +55,4 @@ def post_list(request, format=None):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-   	 	return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
